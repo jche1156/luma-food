0cf1515ff8577df124d15123bddeb47fe5817efd
John Chen <john.hk.chen@gmail.com>
2024-09-14 18:30:42 -0700

    Add event data to experiment with parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde_json::{to_string, Value};
+use std::fs;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let data = fs::read_to_string("src/luma_events.json").expect("Unable to read file");
+    let v: Value = serde_json::from_str(data.as_str())?;
+    for obj in v["objects"].as_array() {
+        for x in obj {
+            for items in x["items"].as_array() {
+                for item in items {
+                    println!("({}, {})", item["title"], item["link"]);
+                }
+            }
+        }
+    }
+    Ok(())
 }
